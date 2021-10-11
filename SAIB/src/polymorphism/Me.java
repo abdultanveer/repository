0daed4fe7b0d269e27c24c8f4f29d0ccfd2ce4c2
abdultanveer <abdul.tanveer@gmail.com>
2021-10-11 16:28:21 +0530

    added student class

--- SAIB/src/polymorphism/Me.java
+++ SAIB/src/polymorphism/Me.java
@@ -5,15 +5,14 @@ import inheritance.IncomeTax;
 
 public class Me {
 	public static void main(String[] args) {
-		//--------------------polymorphism
 		Bank bank;
 		bank = new AlRajhi();
 				//new Saib();
 		int amountRequired = bank.openLocker();
 		System.out.println("amount required to open locker = "+amountRequired);
-		//inheritance
-		IncomeTax incomeTax = //new Gst();
-				new IncomeTax();
+		
+		IncomeTax incomeTax = new Gst();
+				//new IncomeTax();
 		int tax = incomeTax.calculateTax(12000);
 		System.out.println("tax to be paid ="+tax);
 	
